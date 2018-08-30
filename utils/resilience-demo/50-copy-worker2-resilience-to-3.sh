5d14fe023c037f6ae63368a3e6cfe8b7036639cb
Scott Lystig Fritchie <slfritchie@snookles.com>
2018-08-29 21:22:59 -0500

    Add DOS server support to resilience-demo scripts, part 2. IT WORKS for resilience=off!

--- utils/resilience-demo/50-copy-worker2-resilience-to-3.sh
+++ utils/resilience-demo/50-copy-worker2-resilience-to-3.sh
@@ -2,10 +2,25 @@
 
 . ./COMMON.sh
 
-echo
-echo "NOTE: Using the rsync method, TODO"
-echo
+if [ $RESTORE_VIA_JOURNAL_DUMP = y ]; then
+    echo Rsync journal file from DOS server $DOS_SERVER to $SERVER3
+    ssh -n $USER@$SERVER3_EXT "rm -f /tmp/market-spread*"
+    ssh -A -n $USER@$SERVER3_EXT "rsync -raH -v -e 'ssh -o \"StrictHostKeyChecking no\"' ${DOS_SERVER}:/tmp/dos-data/worker2/\* /tmp"
 
-ssh -A -n $USER@$SERVER3_EXT "rsync -raH -v -e 'ssh -o \"StrictHostKeyChecking no\"' ${SERVER2}:/tmp/market-spread\* /tmp"
+    echo Extract journalled I/O ops from the journal file
+    ssh -n $USER@$SERVER3_EXT "cd wallaroo ; python ./utils/journal-dump/journal-dump.py /tmp/market-spread-worker2.journal"
+    ssh -n $USER@$SERVER3_EXT "ls -l /tmp/mar*"
+    sleep 3
+else
+    echo
+    echo "NOTE: rsync all resilience files directly from 'failed' worker (cheating)"
+    echo
+
+    ssh -A -n $USER@$SERVER3_EXT "rsync -raH -v -e 'ssh -o \"StrictHostKeyChecking no\"' ${SERVER2}:/tmp/market-spread\* /tmp"
+fi
+
+echo
+echo "NOTE: Kludge to fix up tcp-control and tcp-data files, TODO"
+echo
 
 ssh -n $USER@$SERVER3_EXT "(echo $SERVER3 ; echo 3131) > /tmp/market-spread-worker2.tcp-control ; (echo $SERVER3 ; echo 3132) > /tmp/market-spread-worker2.tcp-data"
