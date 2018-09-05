ea1fd9654d0f19c1439fcfe15bece241d0b8eba3
Scott Lystig Fritchie <slfritchie@snookles.com>
2018-09-05 15:38:26 -0500

    WIP: consolidate resilience-demo step 10 scripts

--- utils/resilience-demo/10-setup-slf-repo.r=on.sh
+++ utils/resilience-demo/10-setup-slf-repo.r=on.sh
@@ -2,20 +2,6 @@
 
 . ./COMMON.sh
 
-ssh -n $USER@$SERVER1_EXT "git clone $REPO_URL"
-ssh -n $USER@$SERVER1_EXT "cd wallaroo ; git checkout $REPO_BRANCH ; git diff"
+RESILIENCE_FLAG="resilience=on"
 
-if [ "$SKIP_CLEAN" = "" ]; then
-    ssh -n $USER@$SERVER1_EXT "cd wallaroo ; make clean"
-fi
-
-echo ; echo NOTE resilience=on ; echo
-
-if [ "$SKIP_MAKE" = "" ]; then
-    ssh -n $USER@$SERVER1_EXT "cd wallaroo ; make PONYCFLAGS='--verbose=1 -d' resilience=on build-testing-performance-apps-market-spread build-giles-all build-utils-cluster_shutdown"
-fi
-
-for i in $SERVER2 $SERVER3 $SERVER4; do
-    echo rsync to $i
-    ssh -A -n $USER@$SERVER1_EXT "rsync -raH --delete -e 'ssh -o \"StrictHostKeyChecking no\"' ~/wallaroo ${i}:"
-done
+./CLONE-CHECKOUT-MAKE.sh
